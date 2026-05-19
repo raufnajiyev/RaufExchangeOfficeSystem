a5a6e731f7262744bc6046b50d929b667ea0dbdf
Rauf <nciyevrauf@gmail.com>
2026-05-20 00:54:42 +0200

    Add currency service contract

--- WCF-Service/CurrencyExchangeService/IService1.cs
+++ WCF-Service/CurrencyExchangeService/IService1.cs
@@ -1,47 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
+﻿using System.ServiceModel;
 
 namespace CurrencyExchangeService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IService1
     {
+        [OperationContract]
+        decimal GetExchangeRate(string currencyCode);
 
         [OperationContract]
-        string GetData(int value);
+        decimal ConvertCurrency(string fromCurrency, string toCurrency, decimal amount);
 
         [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: Add your service operations here
-    }
-
-
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
+        string[] GetSupportedCurrencies();
     }
 }
